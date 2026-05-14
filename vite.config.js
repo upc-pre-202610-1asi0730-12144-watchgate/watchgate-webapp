afc9493d7ac6f8b0b046c7669b25c4dcd9124514
AlonsoHiga <u202416276@upc.edu.pe>
2026-05-14 18:59:07 -0500

    chore: add github actions for pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    base: '/watchgate-webapp/',
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })
