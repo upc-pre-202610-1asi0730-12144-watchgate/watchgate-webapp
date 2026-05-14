d3da164dbbfb85dc72e9bd2caff6320b38ae1a67
AlonsoHiga <u202416276@upc.edu.pe>
2026-05-14 16:37:42 -0500

    feat(warehouse-management): add environment variables

--- src/iam/infrastructure/iam-api.js
+++ src/iam/infrastructure/iam-api.js
@@ -1,21 +1,26 @@
 import axios from 'axios';
 
 const http = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: import.meta.env.VITE_API_BASE_URL
 });
+
+const USERS_ENDPOINT = import.meta.env.VITE_USERS_ENDPOINT_PATH;
+const COMPANIES_ENDPOINT = import.meta.env.VITE_COMPANIES_ENDPOINT_PATH;
+
 export class IamApi {
     signIn(email, password) {
-        return http.get(`/users?email=${email}&password=${password}`);
+        return http.get(`${USERS_ENDPOINT}?email=${email}&password=${password}`);
     }
 
     signUp(userResource) {
-        return http.post('/users', userResource);
+        return http.post(USERS_ENDPOINT, userResource);
     }
 
     getUserByEmail(email) {
-        return http.get(`/users?email=${email}`);
+        return http.get(`${USERS_ENDPOINT}?email=${email}`);
     }
+
     createCompany(companyResource) {
-        return http.post('/companies', companyResource);
+        return http.post(COMPANIES_ENDPOINT, companyResource);
     }
 }
--- src/warehouse/infrastructure/warehouse.api.js
+++ src/warehouse/infrastructure/warehouse.api.js
@@ -1,27 +1,29 @@
 import axios from 'axios';
 
 const http = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: import.meta.env.VITE_API_BASE_URL
 });
+
+const WAREHOUSES_ENDPOINT = import.meta.env.VITE_WAREHOUSES_ENDPOINT_PATH;
 
 export class WarehouseApi {
     getWarehouses() {
-        return http.get('/warehouses');
+        return http.get(WAREHOUSES_ENDPOINT);
     }
 
     getWarehouseById(id) {
-        return http.get(`/warehouses/${id}`);
+        return http.get(`${WAREHOUSES_ENDPOINT}/${id}`);
     }
 
     createWarehouse(warehouseResource) {
-        return http.post('/warehouses', warehouseResource);
+        return http.post(WAREHOUSES_ENDPOINT, warehouseResource);
     }
 
     updateWarehouse(id, warehouseResource) {
-        return http.put(`/warehouses/${id}`, warehouseResource);
+        return http.put(`${WAREHOUSES_ENDPOINT}/${id}`, warehouseResource);
     }
 
     deleteWarehouse(id) {
-        return http.delete(`/warehouses/${id}`);
+        return http.delete(`${WAREHOUSES_ENDPOINT}/${id}`);
     }
 }
